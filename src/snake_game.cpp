c25ff1b5493aefe9d47648b58305cd17a05f4e19
Wojciech Bartnik <yisonPylkita@gmail.com>
2018-04-25 15:45:01 +0200

    Game loop - copy and paste from internet

--- src/snake_game.cpp
+++ src/snake_game.cpp
@@ -1,9 +1,30 @@
+#include <chrono>
 #include <SFML/Window.hpp>
+
+void update_game(uint32_t) {}
+void render_game_frame() {}
 
 int main()
 {
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+    const sf::Time TimePerFrame = sf::seconds(1.f/60.f);
     sf::Window window(sf::VideoMode(800, 600), "snake");
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+    constexpr uint32_t time_step_in_ms = 16;
     while (window.isOpen()) {
+        sf::Time dt = clock.restart();
+        timeSinceLastUpdate += dt;
+        while (timeSinceLastUpdate > TimePerFrame) {
+            timeSinceLastUpdate -= TimePerFrame;
+            update_game(TimePerFrame.asMilliseconds());
+        }
+
+        // render game world
+        render_game_frame();
+
+        // handle user actions
         sf::Event event{};
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
