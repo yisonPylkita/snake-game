bf7ab301138ea8d14d42d0a61b9b0497ec87a9fc
Wojciech Bartnik <yisonPylkita@gmail.com>
2018-04-25 13:57:13 +0200

    SFML simple window

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    std::cout << "Snake game ^^" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "snake");
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
 }
